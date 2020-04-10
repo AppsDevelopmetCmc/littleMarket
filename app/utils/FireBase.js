bae07ad348c0d3dbed4c6f41067c83d234ba3f6b
Alexander Inca <alex.inca66@gmail.com>
2020-04-09 20:19:28 -0500

    Configuración de facebook y firebase con la nueva base

--- app/utils/FireBase.js
+++ app/utils/FireBase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-   apiKey: 'AIzaSyAbaVy1OIeWYEKIKMRE1QuFCZ18-59wHM4',
-   authDomain: 'little-market-dev.firebaseapp.com',
-   databaseURL: 'https://little-market-dev.firebaseio.com',
-   projectId: 'little-market-dev',
-   storageBucket: 'little-market-dev.appspot.com',
-   messagingSenderId: '188175060398',
-   appId: '1:188175060398:web:08d58eb7bbb974e17fcc3f',
+   apiKey: 'AIzaSyCuPTN-HQyPxrLUr40Bl2nmX5PqNCUVnJg',
+   authDomain: 'little-market-dev-377b6.firebaseapp.com',
+   databaseURL: 'https://little-market-dev-377b6.firebaseio.com',
+   projectId: 'little-market-dev-377b6',
+   storageBucket: 'little-market-dev-377b6.appspot.com',
+   messagingSenderId: '549900659572',
+   appId: '1:549900659572:web:ce8621915b320376469a21',
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
