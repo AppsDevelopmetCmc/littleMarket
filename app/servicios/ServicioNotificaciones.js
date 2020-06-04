09c2cf8694ea3121e77308865a0d27cb1f45e26e
Patricio <samueza.patricio@gmail.com>
2020-06-04 16:15:37 -0500

    Notificaciones 2

--- app/servicios/ServicioNotificaciones.js
+++ app/servicios/ServicioNotificaciones.js
@@ -27,4 +27,48 @@ export class ServicioNotificaciones {
          })
    }
 
+   actualizarNotificaciones = (objeto) => {
+      global
+         .db
+         .collection("notificaciones")
+         .doc(objeto.mail)
+         .update(
+            {
+               numero: objeto.numero,
+            }
+         ).then(function () {
+            Alert.alert("Notificacion actualizado")
+         }
+         ).catch(function (error) {
+            Alert.alert("error" + error)
+         }
+         );
+   }
+
+   registrarEscuchaTodas = (idMail, fnRepintar, mail) => {
+      let arregloUtil = new ArregloUtil(pedido);
+      global.db
+         .collection('notificaciones')
+         .doc(idMail)
+         .collection('notificaciones')
+         .where('mail', '==', mail)
+         .orderBy("orden", "desc")
+         .onSnapshot(function (snapShot) {
+            snapShot.docChanges().forEach(function (change) {
+               let itemPedidos = change.doc.data();
+               itemPedidos.id = change.doc.id;
+
+               if (change.type == 'added') {
+                  arregloUtil.agregar(itemPedidos, fnRepintar);
+               }
+               if (change.type == 'modified') {
+                  arregloUtil.actualizar(itemPedidos, fnRepintar);
+               }
+               if (change.type == 'removed') {
+                  arregloUtil.eliminar(itemPedidos, fnRepintar);
+               }
+            });
+         });
+   };
+
 }
