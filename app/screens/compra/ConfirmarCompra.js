594de9046a6aa6f6a3d76e5d57361e2d26db3318
Patricio <samueza.patricio@gmail.com>
2020-06-08 23:23:51 -0500

    Bugs creados en Carro de Compras

--- app/screens/compra/ConfirmarCompra.js
+++ app/screens/compra/ConfirmarCompra.js
@@ -533,7 +533,7 @@ export class ConfirmarCompra extends Component {
                                     asociado: 'asociado@gmail.com',
                                     nombreAsociado: 'Juan perez',
                                     telefonoAsociado: '1245635',
-                                    yapa: global.yapa.descripcion,
+                                    yapa: global.yapa ? global.yapa.descripcion : "",
                                     descuento: parseFloat(this.state.valorDescuento.toFixed(2))
                                  },
                                  items,
