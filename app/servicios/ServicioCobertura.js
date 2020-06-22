0fac03a14110637c15e4221be454062533321bdc
carolina <carolia.hurtado@clearminds-it.com>
2020-06-22 18:51:45 -0500

    AHU-implentacion servicio cobertura sector

--- app/servicios/ServicioCobertura.js
+++ app/servicios/ServicioCobertura.js
@@ -1,13 +1,12 @@
 import { ArregloUtil } from '../utils/utils';
-import { Alert } from 'react-native';
+
+const URLCOBERTURA =
+   'https://us-central1-little-market-dev-377b6.cloudfunctions.net/coberturaSectores?=latitude';
 
 export class ServicioCobertura {
-
    registrarEscuchaCoberturaTodas = (arreglo, fnObtener) => {
       let arregloUtil = new ArregloUtil(arreglo);
-      global.db
-         .collection('coberturas')
-         .onSnapshot(function(snapShot) {
+      global.db.collection('coberturas').onSnapshot(function (snapShot) {
             snapShot.docChanges().forEach(function(change) {
                let coberturas=change.doc.data();
                coberturas.id=change.doc.id;
@@ -25,18 +24,22 @@ export class ServicioCobertura {
    };
 
    getRegistrarCoberturaTodas = async() => {
-
-     let coleccion=await global.db
-         .collection('coberturas')
-         .get();
+      let coleccion = await global.db.collection('coberturas').get();
          let documentos = coleccion.docs;
             let coberturas = [];
             for (let i = 0; i < documentos.length; i++) {
                coberturas.push(documentos[i].data());
-               
             }
             global.coberturas=coberturas;
-
    };
-   
+
+   consultarCobertura = async (latitud, longitud) => {
+      console.log('INIT CONSULTA COBERTURA');
+      const dir = URLCOBERTURA + '' + latitud + "&" + "longitude=" + longitud;
+      console.log("URL" +dir);
+      let response = await fetch(URLCOBERTURA + '' + latitud + "&" + "longitude=" + longitud);
+      let trama = await response.json();
+      console.log('RESPONSE COBERTURA', trama);
+      return trama;
+   };
 }
