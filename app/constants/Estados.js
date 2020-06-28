ab777978a160719bdc2b6102a5ab24c9ee671541
Alexander Inca <alex.inca66@gmail.com>
2020-06-28 11:23:08 -0500

    Cambio en la pantalla de Pedidos, estilos

--- app/constants/Estados.js
+++ app/constants/Estados.js
@@ -1,13 +1,13 @@
-export const ESTADOS ={
-    PEDIDOS :[
-       { id: 'PI', descripcion:'Buscando Asociado' },
-       { id: 'CT', descripcion:'Confirmando Pago' },
-       { id: 'TR', descripcion:'Pago Rechazado' },
-       { id: 'TA', descripcion:'Buscando Asociado' },
-       { id: 'AA', descripcion:'Pendiente de entrega' },
-       { id: 'IE', descripcion:'Entregando' },
-       { id: 'PE', descripcion:'Finalizado' },
-       { id: 'PC', descripcion:'Finalizado' },
-       { id: 'CA', descripcion:'Cancelado' },
-    ],
+export const ESTADOS = {
+   PEDIDOS: [
+      { id: 'PI', descripcion: 'Buscando Repartidor' },
+      { id: 'CT', descripcion: 'Confirmando Pago' },
+      { id: 'TR', descripcion: 'Pago Rechazado' },
+      { id: 'TA', descripcion: 'Buscando Repartidor' },
+      { id: 'AA', descripcion: 'Pendiente de entrega' },
+      { id: 'IE', descripcion: 'Entregando' },
+      { id: 'PE', descripcion: 'Finalizado' },
+      { id: 'PC', descripcion: 'Finalizado' },
+      { id: 'CA', descripcion: 'Cancelado' },
+   ],
 };
