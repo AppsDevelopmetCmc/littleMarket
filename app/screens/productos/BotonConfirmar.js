3f55dc9ffc8b6d6906d30d3c789ff688e45977b6
carolina <carolia.hurtado@clearminds-it.com>
2020-06-27 22:14:37 -0500

    AHU: implementacion yappa modificaion mapa

--- app/screens/productos/BotonConfirmar.js
+++ app/screens/productos/BotonConfirmar.js
@@ -6,16 +6,22 @@ import {
    Image,
    Alert,
    TouchableOpacity,
+   Modal,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as colores from '../../constants/Colores';
 import { Badge } from 'react-native-elements';
 import * as servPedidos from '../../servicios/ServicioPedidos';
+import { SeleccionarYapa } from '../carroCompras/SeleccionarYappa';
 export class BotonConfirmar extends Component {
    constructor(props) {
       super(props);
+      this.yapas = [];
+      this.tramaYapa = [];
+
       this.state = {
          subtotal: 0,
+         mostrarModalYapa: false,
       };
       if (global.refrescarBotones == null) {
          global.refrescarBotones = [];
@@ -31,6 +37,85 @@ export class BotonConfirmar extends Component {
    navegarConfirmarCompra = () => {
       this.props.navigation.navigate('ConfirmarCompraScreen');
    };
+
+   componentDidMount() {
+      this.todasYapas();
+   }
+
+   todasYapas = async () => {
+      global.db
+         .collection('yapas')
+         .get()
+         .then(querySnapshot => {
+            let documentos = querySnapshot.docs;
+            let yapa = null;
+
+            for (let i = 0; i < documentos.length; i++) {
+               yapa = documentos[i].data();
+               yapa.id = documentos[i].id;
+               this.yapas.push(yapa);
+            }
+            console.log('TODAS YAPAS' + this.yapas);
+         })
+         .catch(error => {
+            response.send('Errorcito' + error);
+         });
+   };
+
+   mostrarModalYapa = async bandera => {
+      this.tramaYapa = [];
+      console.log('Ingesar a Mostrar Modal');
+      console.log('SUBTOTAL' + this.state.subtotal.toFixed(2));
+      if (
+         this.state.subtotal.toFixed(2) >= 10 &&
+         this.state.subtotal.toFixed(2) < 20
+      ) {
+         console.log('ENTRE 10 Y 20');
+         for (var i = 0; i < this.yapas.length; i++) {
+            if (this.yapas[i].tipo == 1) {
+               this.tramaYapa.push(this.yapas[i]);
+            }
+         }
+      }
+
+      if (this.state.subtotal.toFixed(2) >= 20) {
+         console.log('MAS DE 20');
+         for (var i = 0; i < this.yapas.length; i++) {
+            if (this.yapas[i].tipo == 2) {
+               this.tramaYapa.push(this.yapas[i]);
+            }
+         }
+      }
+      this.setState({ mostrarModalYapa: bandera });
+   };
+   validarMonto = () => {
+      if (
+         global.montoYapa >= 10 &&
+         global.montoYapa < 20 &&
+         this.state.subtotal.toFixed(2) >= 10 &&
+         this.state.subtotal.toFixed(2) < 20
+      ) {
+         console.log('MISMA YAPA de 10');
+      } else if (
+         global.montoYapa >= 20 &&
+         this.state.subtotal.toFixed(2) >= 20
+      ) {
+         console.log('MISMA YAPA de 20');
+      } else {
+         global.yapa = undefined;
+      }
+      if (this.state.subtotal.toFixed(2) < 10) {
+         Alert.alert('Información', 'Monto mínimo de compra $10.00');
+         return;
+      } else if (this.state.subtotal.toFixed(2) >= 10 && !global.yapa) {
+         global.montoYapa = this.state.subtotal.toFixed(2);
+         this.mostrarModalYapa(true);
+         return;
+      } else if (global.yapa) {
+         this.props.navigation.navigate('ConfirmarCompraScreen');
+         return;
+      }
+   };
    render() {
       console.log('Repinta el botón: ', this.state.subtotal);
       return (
@@ -44,8 +129,8 @@ export class BotonConfirmar extends Component {
                      latitud: 0.0,
                      longitud: 0.0,
                   };
-                  global.yapa = { descripcion: 'PENDIENTE ' };
-                  this.navegarConfirmarCompra();
+                  //yappa
+                  this.validarMonto();
                }}
             >
                <View
@@ -118,6 +203,17 @@ export class BotonConfirmar extends Component {
                   </View>
                </View>
             </TouchableOpacity>
+            <Modal
+               animationType="slide"
+               transparent={true}
+               visible={this.state.mostrarModalYapa}
+            >
+               <SeleccionarYapa
+                  mostrarModal={this.mostrarModalYapa}
+                  listaYapa={this.tramaYapa}
+                  navigation={this.props.navigation}
+               ></SeleccionarYapa>
+            </Modal>
          </View>
       );
    }
